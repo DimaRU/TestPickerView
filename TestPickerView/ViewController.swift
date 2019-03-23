0bffaa13b1e347a19263f17ac3b18a087b9fd1bf
Dmitriy Borovikov <dmitry@borovikov.name>
2019-03-23 10:20:12 +0300

    Select photo

--- TestPickerView/ViewController.swift
+++ TestPickerView/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePickerController = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupPickerController()
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    @IBAction func selectPhotoButtonTap(_ sender: Any) {
+        present(imagePickerController, animated: true)
+    }
+    
+    
+    private func setupPickerController() {
+        imagePickerController.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePickerController.sourceType = .camera
+            if UIImagePickerController.isCameraDeviceAvailable(.rear) {
+                imagePickerController.cameraDevice = .rear
+            }
+        } else if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            imagePickerController.sourceType = .photoLibrary
+        } else if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+            imagePickerController.sourceType = .savedPhotosAlbum
+        } else {
+            print("Nothing avaible")
+        }
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        defer {
+            imagePickerController.dismiss(animated: true)
+        }
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            print("Strange: no image aviable")
+            return
+        }
+        DispatchQueue.main.async {
+            self.imageView.image = image
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("Cancelled")
+    }
+}
+
+extension ViewController: UINavigationControllerDelegate {
+    
+}
