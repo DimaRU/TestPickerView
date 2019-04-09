3fca480e1593c02502cd45804db2f5a00ec70e39
Dmitriy Borovikov <dmitry@borovikov.name>
2019-04-09 15:13:18 +0300

    Demo with animation

--- TestPickerView/ViewController.swift
+++ TestPickerView/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
-import AVFoundation
+import Photos
+import PromiseKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
 
+    private var phassets: [PHAsset] = []
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,7 +21,28 @@ class ViewController: UIViewController {
     @IBAction func selectPhotoButtonTap(_ sender: Any) {
         let storyboard = UIStoryboard(name: "PickPhotoController", bundle: nil)
         let pickPhotoController = storyboard.instantiateInitialViewController() as! PickPhotoController
-        present(pickPhotoController, animated: true)
+        pickPhotoController.delegate = self
+        navigationController?.pushViewController(pickPhotoController, animated: true)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        guard !phassets.isEmpty else { return }
+        imageView.animationImages = []
+        let promises = phassets.map {
+            PHImageManager.default().requestFullImage(for: $0).done { self.imageView.animationImages?.append($0) }
+        }
+        when(fulfilled: promises)
+            .done {
+                self.imageView.animationDuration = 0.5 * Double(self.imageView.animationImages?.count ?? 0)
+                self.imageView.startAnimating()
+            }.catch { print($0) }
+    }
+    
+}
+
+extension ViewController: PickPhotoControllerProtocol {
+    func selected(assets: [PHAsset]) {
+        phassets = assets
+    }
 }
